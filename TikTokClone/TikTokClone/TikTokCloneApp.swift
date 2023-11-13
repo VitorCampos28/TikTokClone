00fa29d0e47079f01fc1ebb7ab5399242b559a78
Vitor Campos <vitor.natal@timwe.com>
2023-11-13 17:59:29 -0300

    first commit

---
+++ TikTokClone/TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Vitor Campos on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
